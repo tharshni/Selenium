607c6f8990774caac722a10a3ae781132e6695a7
Idhaz <Idhaz@192.168.1.188>
2022-08-30 08:39:06 +0530

    fourth commit

---
+++ Selenium Training/src/Basics/Day2Variables2.java
@@ -0,0 +1,18 @@
+package Basics;
+
+/*import java.security.PublicKey;*/
+
+public class Day2Variables2 {
+	public static void main(String[] args) throws InterruptedException {
+		
+	/*string Name="K.Priyatharshni";*/
+	char gender='F';
+	int age=32;
+	float height=159;
+	double weight=60;
+	boolean result=true;
+	System.out.println("The gender is:" +gender);
+	Thread.sleep(30000);
+				}
+
+}
